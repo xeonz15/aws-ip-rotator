18e4831434f9b9e05bfaca2be162444453a58236
Maksim Mulyanov <xeonz73@gmail.com>
2020-01-10 15:33:35 +0400

    fix for vpc EIPs

--- aws-ip-rotator.sh
+++ aws-ip-rotator.sh
@@ -10,13 +10,17 @@ case $1 in
     *)
 	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 	OLD_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-	NEW_IP=$(aws ec2 allocate-address --query PublicIp | tr -d '"')
+	NEW_IP=$(aws ec2 allocate-address --query PublicIp --profile elastic-ip | tr -d '"')
 	echo ''
 	echo "Old IP: $OLD_IP"
 	echo "New IP: $NEW_IP"
 	echo ''
 	echo 'Associating new IP...'
-	aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP
-	echo 'Releasing old IP...'
-	aws ec2 release-address --public-ip $OLD_IP
+	aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP --profile elastic-ip
+	#echo 'Releasing old IP...'
+	echo "Trying to release $OLD_IP, getting allocation id..."
+	ALLOC_ID=$(aws ec2 describe-addresses --public-ips $OLD_IP --profile elastic-ip --query 'Addresses[].AllocationId' --output text)
+	echo "Releasing IP with allocation id $ALLOC_ID..."
+	aws ec2 release-address --allocation-id  $ALLOC_ID  --profile elastic-ip
+	echo "Done"
 esac
